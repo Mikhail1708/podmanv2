c47778e75dbbd470fce757960205c8db08cbd36f
Mikhail1708 <mikhail52gr@gmail.com>
2025-05-17 09:16:33 +0800

    Delete genpdf directory

--- genpdf/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from integration_framework.views import generate_ticket
-
-urlpatterns = [
-    path('api/generate-ticket/', generate_ticket, name='generate_ticket'),
-
-]
