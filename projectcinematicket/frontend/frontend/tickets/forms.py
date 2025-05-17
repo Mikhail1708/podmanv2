54151c96aac405f66380244ee0bddcbe83990677
Mikhail1708 <mikhail52gr@gmail.com>
2025-05-17 09:17:55 +0800

    Delete projectcinematicket/frontend/frontend directory

--- projectcinematicket/frontend/frontend/tickets/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-ROW_CHOICES = [(str(i), f'Ряд {i}') for i in range(1, 11)]  # ряды 1-10
-SEAT_CHOICES = [(str(i), f'Место {i}') for i in range(1, 21)]  # места 1-20
-
-class TicketForm(forms.Form):
-    row = forms.ChoiceField(label='Ряд', choices=ROW_CHOICES)
-    seat = forms.ChoiceField(label='Место', choices=SEAT_CHOICES)
-    name = forms.CharField(label='ФИО')
--- projectcinematicket/frontend/frontend/tickets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('buy/', views.buy_ticket, name='buy_ticket'),
-
-]
